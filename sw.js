2bcee5792716937f33dbb286ca2e029bfd5ce71d
NOTAS <wrmr90@gmail.com>
2026-09-24 21:19:29 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Guarda la app para que funcione sin internet. Nunca guarda tus notas: esas viven cifradas en el almacenamiento del navegador.
-const CACHE = 'bitacora-v1';
+const CACHE = 'bitacora-v3';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
